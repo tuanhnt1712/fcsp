ecdaa5e18ca7c92713fd480ac5d34e3fb40239a3
thang <nguyenthe.thang1995@gmail.com>
2017-12-15 15:56:56 +0700

    test model user course subject

--- app/models/user_course_subject.rb
+++ app/models/user_course_subject.rb
@@ -1,10 +1,10 @@
 class UserCourseSubject < ApplicationRecord
-
   belongs_to :user
   belongs_to :subject
   belongs_to :course
 
   enum status: %i(init in_progress finished closed)
+
   delegate :name, to: :subject, prefix: true
   delegate :description, to: :subject, prefix: true
 
--- spec/models/user_course_subject_spec.rb
+++ spec/models/user_course_subject_spec.rb
@@ -8,7 +8,15 @@ RSpec.describe UserCourseSubject, type: :model do
   end
 
   context "enum" do
-    it{is_expected.to define_enum_for(:status)}
+    it do
+      is_expected.to define_enum_for(:status)
+        .with %i(init in_progress finished closed)
+    end
+  end
+
+  context "delegate" do
+    it{is_expected.to delegate_method(:name).to(:subject).with_prefix}
+    it{is_expected.to delegate_method(:description).to(:subject).with_prefix}
   end
 
   context "column_specifications" do
@@ -18,5 +26,27 @@ RSpec.describe UserCourseSubject, type: :model do
     it{is_expected.to have_db_column(:start_date).of_type :datetime}
     it{is_expected.to have_db_column(:end_date).of_type :datetime}
     it{is_expected.to have_db_column(:status).of_type :integer}
+    it{is_expected.to have_db_column(:content).of_type :string}
+  end
+
+  context "test scope check_user " do
+    let!(:user){FactoryBot.create :user}
+    let!(:user2){FactoryBot.create :user}
+    let!(:course){FactoryBot.create :course}
+    let!(:subject){FactoryBot.create :subject}
+    let!(:user_course_subject) do
+      FactoryBot.create :user_course_subject, user_id: user.id,
+        course_id: course.id, subject_id: subject.id
+    end
+
+    it "matching successfully" do
+      expect(UserCourseSubject.check_user user)
+        .to include user_course_subject
+    end
+
+    it "matching not successfully" do
+      expect(UserCourseSubject.check_user user2)
+        .not_to include user_course_subject
+    end
   end
 end
